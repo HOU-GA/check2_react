fc4e375dc881cebdf36eb4ce5b467ba06bf3c893
HOU-GA <iyedfamille6@gmail.com>
2025-07-13 22:45:28 +0200

    chek2 react

---
+++ src/product.js
@@ -0,0 +1,17 @@
+export const product = {
+    name : "TISSOT T1504171104100",
+    price : "1 461,000 TND",
+    description : `Montre Homme Tissot T1504171104100
+Forme du cadran: Ronde
+Largeur de boitier : 40 mm
+Couleur de cadran : Bleu
+Chronographe : Fonctionnel
+Couleur de boitier: Argent
+Matière de boitier: Acier inoxydable
+Couleur bracelet : Argent
+Matière de bracelet : Acier inoxydable
+Résistance à l'eau : 10 ATM
+Garantie :  2 ans`,
+    imgURL : "https://citywatch.com.tn/39181-large_default/tissot-t1504171104100.jpg",
+
+}
